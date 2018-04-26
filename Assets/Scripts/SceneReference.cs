d706e2abe609985b2bf76856b8481a6cb83d9eda
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2018-04-26 03:42:53 +0300

    Added Scene Reference

---
+++ Assets/Scripts/SceneReference.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Hirame {
+
+    [CreateAssetMenu (menuName = "Hirame/Scene Reference")]
+    public class SceneReference : ScriptableObject {
+
+#if UNITY_EDITOR
+        public SceneAsset Scene;
+#endif
+
+        public void LoadScene () {
+            LoadScene (LoadSceneMode.Single);
+        }
+
+        public void LoadScene (LoadSceneMode loadSceneMode) {
+            SceneManager.LoadScene (Scene.name, loadSceneMode);
+        }
+    }
+
+}
