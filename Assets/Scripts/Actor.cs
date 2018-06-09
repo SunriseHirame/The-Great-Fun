479f4c341e4baff956ebca630bec24fc5a1873ab
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2018-06-10 01:04:28 +0300

    asd

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -7,6 +7,7 @@ namespace Hirame {
 
     public class Actor : MonoBehaviour {
 
+        public Faction ActorFaction;
         public ResourceAttribute Health;
 
         public UnityEvent ActorDeath;
---
+++ Assets/Scripts/Faction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hirame {
+
+    [CreateAssetMenu (menuName = "Hirame/Faction")]
+    public class Faction : ScriptableObject {
+
+    }
+
+}
